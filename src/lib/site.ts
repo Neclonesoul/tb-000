ecae4fa75f51e0d9713ad28af18dedf08a1c9afd
Neclonesoul <103559137+Neclonesoul@users.noreply.github.com>
2026-09-01 14:49:51 +0200

    Add primary navigation and social identity links

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -5,13 +5,22 @@ export const SITE = {
   url: 'https://tysonbarnes.co.uk',
   github: 'https://github.com/Neclonesoul',
   email: 'mailto:tyson@darktech.co.za',
-  location: 'South Africa / United Kingdom'
+  location: 'South Africa / United Kingdom',
+  socials: {
+    x: 'https://x.com/tysonbarnes',
+    youtube: 'https://www.youtube.com/@TysonBarnesOfficial',
+    instagram: 'https://www.instagram.com/barnestd2169/',
+    flickr: 'https://www.flickr.com/photos/tysonbarnes/',
+    flickrProfile: 'https://www.flickr.com/people/tysonbarnes/'
+  }
 } as const;
 
 export const primaryNav = [
   { label: 'WORK', href: '/work/' },
   { label: 'FIELD', href: '/field/' },
   { label: 'NOTES', href: '/notes/' },
+  { label: 'SYSTEMS', href: '/systems/' },
+  { label: 'PHOTOGRAPHY', href: '/photography/' },
   { label: 'ABOUT', href: '/about/' }
 ] as const;
 
